e4ad6348287df89721b7be9855352795d17bb91b
Conrad Davis Jr <conrad.davis@akqa.com>
2016-02-10 12:29:34 -0800

    copy updates

--- responsive-image-handler/BP_img_handler.js
+++ responsive-image-handler/BP_img_handler.js
@@ -47,26 +47,21 @@ window.onload = function(){
 			var vpWidth = RS_img_handler.vpWidth = (window.innerWidth > 0) ? window.innerWidth : screen.width;
 			var viewPort = RS_img_handler.breakPoints;
 			var imgSrc = RS_img_handler.imgSrc;
-			console.log('RS_img_handler.vpWidth' + RS_img_handler.vpWidth);
 
 			/*---------------------------------
 			determine which break point were on
 			-----------------------------------*/
 			if(vpWidth >= viewPort.XL.scale){ //XL
 				RS_img_handler.imgSrc = viewPort.XL.name;
-				console.log("imgSrc: " + imgSrc);
 			}else
 			if(vpWidth < viewPort.XL.scale && vpWidth > viewPort.M.scale){ //Large
 				RS_img_handler.imgSrc = viewPort.L.name;
-				console.log("imgSrc: " + imgSrc);
 			}else
 			if(vpWidth <= viewPort.M.scale && vpWidth > viewPort.S.scale){ //Medium
 				RS_img_handler.imgSrc = viewPort.M.name;
-				console.log("imgSrc: " + imgSrc);
 			}else
 			if(vpWidth <= viewPort.S.scale){ //Small
 				RS_img_handler.imgSrc = viewPort.S.name;
-				console.log("imgSrc: " + imgSrc);
 			}
 			//Call the imgUpdater function to update the imgs in the DOM
 			RS_img_handler.imgUpdater();
